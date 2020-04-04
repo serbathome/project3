e8a0f271551d24332d5b8f1c60dbd21c8a2a2589
Sergey Birukov <serb@microsoft.com>
2020-04-04 15:45:02 +0300

    Initial commit

--- orders/views.py
+++ orders/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "index.html", {})
